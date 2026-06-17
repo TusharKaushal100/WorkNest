cd6a81535023e8a082cacc844ef9a63cc4f9b091
Tushar Kaushal <kartikkaushal666@gmail.com>
2026-06-18 02:59:44 +0530

    completed expense router

--- server/src/controller/controller.expense.js
+++ server/src/controller/controller.expense.js
@@ -1,3 +1,4 @@
+import { includes } from 'zod';
 import Prisma from '../config/db.js'
 
 export const submitExpense = async (req,res,next)=>{
@@ -32,3 +33,100 @@ export const submitExpense = async (req,res,next)=>{
       next(err);
    } 
 }
+
+export const getExpense = async (req,res,next)=>{
+  
+    try{
+       let whereClause;
+     
+    if(req.user?.role == "ADMIN"){
+        whereClause = {where:{orgId:req.user?.orgId}}
+    }else{
+        whereClause = {where:{orgId: req.user?.orgId,
+                      userId: req.user?.userId}}
+    }
+
+    const findExpense = await Prisma.expense.findMany(whereClause,
+        {includes:{
+            user:{select:{id:true,name:true,email:true,role:true}},
+            category:{select:{id:true,name:true,budget:true}}
+        }
+        ,
+         orderBy: { createdAt: 'desc' }
+       }
+       
+    );
+
+    if(!findExpense){
+        return res.status(400).json({message:"No data found"});
+    }
+
+    return res.status(200).json({findExpense});
+    }catch(err){
+        next(err);
+    }
+   
+}
+
+export const updateExpenseStatus = async (req,res,next)=>{
+
+    try{
+      
+         const {id} = req.params;
+    const {status} = req.body;
+
+    if(status !== "APPROVED" &&  status !== "REJECTED"){
+          return res.status(400).json({message:"The status should be either approved or rejected"});
+    }
+
+    const currExpense = await Prisma.expense.findFirst({
+            where:{
+              id : id,
+              orgId:req.user?.orgId   
+            }
+
+    });
+
+    if(!currExpense){
+        return res.status(400).json({message:"Expense not found"});
+    }
+    
+    if(currExpense.status !== "PENDING"){
+          return res.status(400).json({message:"Only pending status can be updated."})
+    }
+
+    await Prisma.expense.update(
+         {
+            where:{
+                id:id,
+                orgId:req.user?.orgId
+            },
+            data:{
+                status
+            }
+         }
+    );
+
+    await Prisma.auditLog.create({
+        data:{
+            orgId : req.user?.id,
+            auditorId : req.user?.id,
+            action : `${status} expense`,
+            targetId : id
+
+        }
+    })
+
+    return res.status(200).json({message:`The status is successfullu update to ${status}`});
+    
+}catch(err){
+    next(err);
+}
+
+   
+
+}
+
+
+
+
