a1f224774dc3ae44bc879279a82f0285c59091fe
Tushar Kaushal <kartikkaushal666@gmail.com>
2026-07-23 23:33:18 +0530

    email to Ipv4 fix

--- server/src/services/services.email.js
+++ server/src/services/services.email.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
     secure:true,
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    family: 4,  
     auth:{
         user:process.env.SMTP_USER,
         pass:process.env.SMTP_PASS
